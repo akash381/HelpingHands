0f4e5e547c4e3b9e46f474211fe847e49603b318
Akash Kumar <akash@Akashs-MacBook-Air.local>
2019-01-01 16:56:22 +0530

    initial work

---
+++ app/src/main/java/com/example/akash/findamaid/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.akash.findamaid.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.akash.findamaid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
